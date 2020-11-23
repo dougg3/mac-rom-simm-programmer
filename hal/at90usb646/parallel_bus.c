29e0c7ec1fe884e54d71f18f16028746b6f9ddd9
Doug Brown <doug@downtowndougbrown.com>
2020-11-22 22:28:47 -0800

    Add/update copyright
    This just makes sure everything is up to date with copyrights.

--- hal/at90usb646/parallel_bus.c
+++ hal/at90usb646/parallel_bus.c
@@ -4,6 +4,24 @@
  *  Created on: Nov 26, 2011
  *      Author: Doug
  *
+ * Copyright (C) 2011-2020 Doug Brown
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * -----------------------------------------------------------------------------
+ *
  * For some reason, avr-gcc is super inefficient dealing with uint32_t
  * variables. Looking at the individual bytes using a union results in much
  * more optimized code. Every cycle counts for this. So several of these
